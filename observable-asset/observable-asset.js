82d44718585695a1f6b8dcb2f8a4690c3b631666
Charles Steinmetz <charlessteinmetzk@gmail.com>
2022-02-09 13:21:16 +0100

    adding asset node

---
+++ observable-asset/observable-asset.js
@@ -0,0 +1,11 @@
+module.exports = function(RED) {
+    function ObservableAsset(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("observable-asset",ObservableAsset);
+}
