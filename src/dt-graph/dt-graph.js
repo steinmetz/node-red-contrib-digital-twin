af2bfe9e5207035aa323d62edf5bd8b40528d4bd
Charles Steinmetz <charlessteinmetzk@gmail.com>
2022-04-02 22:41:11 +0200

    graph using new event channel

--- src/dt-graph/dt-graph.js
+++ src/dt-graph/dt-graph.js
@@ -1,9 +1,13 @@
 "use strict";
+var dt_1 = require("../core/dt");
 module.exports = function (RED) {
     function DTGraph(config) {
         RED.nodes.createNode(this, config);
-        this.on('input', function (msg, send, done) {
-            send(msg);
+        // this.on('input', (msg: any, send, done): void => {
+        //     send(msg);
+        // });
+        dt_1.DT.events.on(dt_1.DT.eventNames.updateAsset, function (msg) {
+            console.log(msg);
         });
     }
     ;
--- src/dt-graph/dt-graph.ts
+++ src/dt-graph/dt-graph.ts
@@ -1,4 +1,5 @@
 import * as nodered from "node-red"
+import { DT } from '../core/dt';
 
 interface DTActionNodeDef extends nodered.NodeDef {
     bound_to: string
@@ -7,10 +8,11 @@ interface DTActionNodeDef extends nodered.NodeDef {
 export = (RED: nodered.NodeAPI): void => {
     function DTGraph(this: nodered.Node, config: DTActionNodeDef): void {
         RED.nodes.createNode(this, config);
-
-        this.on('input', (msg: any, send, done): void => {
-            send(msg);
+        
+        DT.events.on(DT.eventNames.updateAsset, (msg: any) => {
+            console.log(msg);
         });
+
     };
     RED.nodes.registerType('dt-graph', DTGraph);
 };
