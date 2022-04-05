9838e5f9013210290bc49e20703c8a113a3ca9ff
Charles Steinmetz <charlessteinmetzk@gmail.com>
2022-04-05 08:48:14 +0200

    adds class for property node

--- src/dt-property/dt-property.js
+++ src/dt-property/dt-property.js
@@ -1,9 +1,16 @@
 "use strict";
 module.exports = function (RED) {
     function DTProperty(config) {
+        var _this = this;
         RED.nodes.createNode(this, config);
         this.on('input', function (msg, send, done) {
-            send(msg);
+            var _a;
+            var data = {
+                propertyName: (_a = _this.name) !== null && _a !== void 0 ? _a : '',
+                propertyValue: msg.payload,
+                propertyId: _this.id,
+            };
+            send(data);
         });
     }
     ;
--- src/dt-property/dt-property.ts
+++ src/dt-property/dt-property.ts
@@ -4,13 +4,24 @@ interface DTPropertyNodeDef extends nodered.NodeDef {
     bound_to: string
 }
 
+interface DTPropertyNodeMessage extends nodered.NodeMessage {
+    propertyName: string,
+    propertyValue: any,
+    propertyId: string,
+}
+
 export = (RED: nodered.NodeAPI): void => {
     function DTProperty(this: nodered.Node, config: DTPropertyNodeDef): void {
         RED.nodes.createNode(this, config);
-
         this.on('input', (msg: any, send, done): void => {
-            send(msg);
+            let data: DTPropertyNodeMessage = {
+                propertyName: this.name ?? '',
+                propertyValue: msg.payload,
+                propertyId: this.id,
+            };
+            send(data);
         });
+
     };
     RED.nodes.registerType('dt-property', DTProperty);
 };
