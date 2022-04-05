e990fa01939bfc82623ec38a21fb7df93555829b
Charles Steinmetz <charlessteinmetzk@gmail.com>
2022-04-05 08:53:52 +0200

    graph endpoint that sends data in cypher format every deploy

--- src/dt-graph/dt-graph.js
+++ src/dt-graph/dt-graph.js
@@ -1,13 +1,83 @@
 "use strict";
-var dt_1 = require("../core/dt");
+var dt_1 = require("../resources/dt");
+var node;
 module.exports = function (RED) {
     function DTGraph(config) {
         RED.nodes.createNode(this, config);
-        // this.on('input', (msg: any, send, done): void => {
-        //     send(msg);
-        // });
+        node = this;
+        RED.httpNode.post('/dt-graph', function (req, res) {
+            if (req.body.action == 'deploy') {
+                var nodes = JSON.parse(req.body.nodes);
+                var assets = nodes.filter(function (node) { return node.type == 'dt-asset'; });
+                var properties = nodes.filter(function (node) { return node.type == 'dt-asset'; });
+                var methods = nodes.filter(function (node) { return node.type == 'dt-asset'; });
+                for (var _i = 0, methods_1 = methods; _i < methods_1.length; _i++) {
+                    var method = methods_1[_i];
+                }
+                var query;
+                var _loop_1 = function (asset) {
+                    var cypher = "MERGE (a:Asset {nodered_id: '".concat(asset.id, "'}) \n                                    SET a.name = '").concat(asset.name, "',\n                                        a.nodered_type = '").concat(asset.type, "'");
+                    var connectedNodes = nodes.filter(function (node) { return asset.wires[0].includes(node.id); });
+                    var label = 0;
+                    var _loop_2 = function (connectedNode) {
+                        if (connectedNode.type == 'dt-property') {
+                            cypher += "\nMERGE (p".concat(label, ":Property {nodered_id: '").concat(connectedNode.id, "'}) \n                                        MERGE (a)-[:hasProperty]->(p").concat(label, ") \n                            SET p").concat(label, ".name = '").concat(connectedNode.name, "',\n                                p").concat(label, ".nodered_type = '").concat(connectedNode.type, "'");
+                        }
+                        else if (connectedNode.type == 'dt-action') {
+                            cypher += "\nMERGE (a".concat(label, ":Action {nodered_id: '").concat(connectedNode.id, "'}) \n                            MERGE (a)-[:hasAction]->(a").concat(label, ") \n                            SET a").concat(label, ".name = '").concat(connectedNode.name, "',\n                                a").concat(label, ".nodered_type = '").concat(connectedNode.type, "'");
+                        }
+                        else if (connectedNode.type == 'dt-relation') {
+                            var relationTarget = nodes.filter(function (node) { return connectedNode.wires[0].includes(node.id); });
+                            for (var _e = 0, relationTarget_2 = relationTarget; _e < relationTarget_2.length; _e++) {
+                                var target = relationTarget_2[_e];
+                                cypher += "\nMERGE (t".concat(label, ":Asset {nodered_id: '").concat(target.id, "'})\n                                MERGE (a)-[:").concat(connectedNode.name, "]->(t").concat(label, ")\n                                SET t").concat(label, ".name = '").concat(target.name, "',\n                                    t").concat(label, ".nodered_type = '").concat(target.type, "'");
+                                label++;
+                            }
+                        }
+                        label++;
+                    };
+                    for (var _b = 0, connectedNodes_1 = connectedNodes; _b < connectedNodes_1.length; _b++) {
+                        var connectedNode = connectedNodes_1[_b];
+                        _loop_2(connectedNode);
+                    }
+                    var nodesConnectedToAsset = nodes.filter(function (node) { return node.wires[0].includes(asset.id); });
+                    for (var _c = 0, nodesConnectedToAsset_1 = nodesConnectedToAsset; _c < nodesConnectedToAsset_1.length; _c++) {
+                        var node_1 = nodesConnectedToAsset_1[_c];
+                        if (node_1.type == 'dt-property') {
+                            cypher += "\nMERGE (p".concat(label, ":Property {nodered_id: '").concat(node_1.id, "'})\n                                MERGE (a)-[:hasProperty]->(p").concat(label, ")\n                                SET p").concat(label, ".name = '").concat(node_1.name, "',\n                                    p").concat(label, ".nodered_type = '").concat(node_1.type, "'");
+                        }
+                        else if (node_1.type == 'dt-action') {
+                            cypher += "\nMERGE (a".concat(label, ":Action {nodered_id: '").concat(node_1.id, "'})\n                                MERGE (a)-[:hasAction]->(a").concat(label, ")\n                                SET a").concat(label, ".name = '").concat(node_1.name, "',\n                                    a").concat(label, ".nodered_type = '").concat(node_1.type, "'");
+                        }
+                        else if (node_1.type == 'dt-relation') {
+                            var relationTarget = nodes.filter(function (node) { return node.wires[0].includes(node.id); });
+                            for (var _d = 0, relationTarget_1 = relationTarget; _d < relationTarget_1.length; _d++) {
+                                var target = relationTarget_1[_d];
+                                cypher += "\nMERGE (t".concat(label, ":Asset {nodered_id: '").concat(target.id, "'})\n                                    MERGE (a)-[:").concat(node_1.name, "]->(t").concat(label, ")\n                                    SET t").concat(label, ".name = '").concat(target.name, "',\n                                        t").concat(label, ".nodered_type = '").concat(target.type, "'");
+                                label++;
+                            }
+                        }
+                        label++;
+                    }
+                    cypher += "\nRETURN a";
+                    query = {
+                        query: cypher,
+                        payload: cypher,
+                    };
+                    node.send(query);
+                };
+                for (var _a = 0, assets_1 = assets; _a < assets_1.length; _a++) {
+                    var asset = assets_1[_a];
+                    _loop_1(asset);
+                }
+            }
+            if (req.body.action == 'node_deleted') {
+            }
+            if (req.body.action == 'node_added') {
+            }
+        });
         dt_1.DT.events.on(dt_1.DT.eventNames.updateAsset, function (msg) {
-            console.log(msg);
+            // console.log(msg);
         });
     }
     ;
--- src/dt-graph/dt-graph.ts
+++ src/dt-graph/dt-graph.ts
@@ -1,16 +1,115 @@
 import * as nodered from "node-red"
-import { DT } from '../core/dt';
+import { DT } from '../resources/dt';
 
 interface DTActionNodeDef extends nodered.NodeDef {
     bound_to: string
 }
 
+interface Node {
+    id: string,
+    type: string,
+    name: string,
+    wires: string[],
+    direction: string,
+}
+interface QueryMessage extends nodered.NodeMessage {
+    query: string
+}
+
+var node: nodered.Node;
+
 export = (RED: nodered.NodeAPI): void => {
     function DTGraph(this: nodered.Node, config: DTActionNodeDef): void {
         RED.nodes.createNode(this, config);
-        
+
+        node = this;
+
+        RED.httpNode.post('/dt-graph', (req, res) => {
+
+            if (req.body.action == 'deploy') {
+
+                let nodes = JSON.parse(req.body.nodes) as Node[];
+                let assets = nodes.filter((node) => node.type == 'dt-asset');
+                var query: QueryMessage;
+
+                // for each asset, creates a query with its relations
+                for (let asset of assets) {
+
+                    let cypher = `MERGE (a:Asset {nodered_id: '${asset.id}'}) 
+                                    SET a.name = '${asset.name}',
+                                        a.nodered_type = '${asset.type}'`;
+
+                    let connectedNodes = nodes.filter((node) => asset.wires[0].includes(node.id));
+
+                    let label = 0;
+
+                    for (let connectedNode of connectedNodes) {
+                        if (connectedNode.type == 'dt-property') {
+                            cypher += `\nMERGE (p${label}:Property {nodered_id: '${connectedNode.id}'}) 
+                                        MERGE (a)-[:hasProperty]->(p${label}) 
+                            SET p${label}.name = '${connectedNode.name}',
+                                p${label}.nodered_type = '${connectedNode.type}'`;
+                        } else if (connectedNode.type == 'dt-action') {
+                            cypher += `\nMERGE (a${label}:Action {nodered_id: '${connectedNode.id}'}) 
+                            MERGE (a)-[:hasAction]->(a${label}) 
+                            SET a${label}.name = '${connectedNode.name}',
+                                a${label}.nodered_type = '${connectedNode.type}'`;
+                        } else if (connectedNode.type == 'dt-relation') {
+                            let relationTarget = nodes.filter((node) => connectedNode.wires[0].includes(node.id));
+                            for (let target of relationTarget) {
+                                cypher += `\nMERGE (t${label}:Asset {nodered_id: '${target.id}'})
+                                MERGE (a)-[:${connectedNode.name}]->(t${label})
+                                SET t${label}.name = '${target.name}',
+                                    t${label}.nodered_type = '${target.type}'`;
+                                label++;
+                            }
+                        }
+                        label++;
+                    }
+                    let nodesConnectedToAsset = nodes.filter((node) => node.wires[0].includes(asset.id));
+                    for (let node of nodesConnectedToAsset) {
+                        if (node.type == 'dt-property') {
+                            cypher += `\nMERGE (p${label}:Property {nodered_id: '${node.id}'})
+                                MERGE (a)-[:hasProperty]->(p${label})
+                                SET p${label}.name = '${node.name}',
+                                    p${label}.nodered_type = '${node.type}'`;
+                        } else if (node.type == 'dt-action') {
+                            cypher += `\nMERGE (a${label}:Action {nodered_id: '${node.id}'})
+                                MERGE (a)-[:hasAction]->(a${label})
+                                SET a${label}.name = '${node.name}',
+                                    a${label}.nodered_type = '${node.type}'`;
+                        } else if (node.type == 'dt-relation') {
+                            let relationTarget = nodes.filter((node) => node.wires[0].includes(node.id));
+                            for (let target of relationTarget) {
+                                cypher += `\nMERGE (t${label}:Asset {nodered_id: '${target.id}'})
+                                    MERGE (a)-[:${node.name}]->(t${label})
+                                    SET t${label}.name = '${target.name}',
+                                        t${label}.nodered_type = '${target.type}'`;
+                                label++;
+                            }
+                        }
+                        label++;
+                    }
+                    cypher += `\nRETURN a`;
+                    query = {
+                        query: cypher,
+                        payload: cypher,
+                    };
+                    node.send(query);
+                }
+            }
+
+            if (req.body.action == 'node_deleted') {
+                //TODO: delete node from db 
+                //      or keep it in memory for when deploy is called
+            }
+            if (req.body.action == 'node_added') {
+
+            }
+        });
+
         DT.events.on(DT.eventNames.updateAsset, (msg: any) => {
-            console.log(msg);
+            //TODO: update asset in graph
         });
 
     };
